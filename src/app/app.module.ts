e67430e9d7eb0393f2976b1d033f8f94af47c51a
berketonoz <btonoz@sabanciuniv.edu>
2023-10-17 03:59:05 +0300

    Angular housing tutorial all steps implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [],
+    providers: [],
+    bootstrap: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HomeComponent,
+        RouterModule,
+    ]
 })
 export class AppModule { }
